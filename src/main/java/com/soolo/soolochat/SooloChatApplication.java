f70edad7f442965ffee76b81ce1a80a816cd2ab4
choihana <chhana0703@gmail.com>
2023-07-18 00:52:15 +0900

    feat:docker 설정

--- src/main/java/com/soolo/soolochat/SooloChatApplication.java
+++ src/main/java/com/soolo/soolochat/SooloChatApplication.java
@@ -2,10 +2,11 @@ package com.soolo.soolochat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@SpringBootApplication
+// @EnableJpaAuditing
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SooloChatApplication {
 
 	public static void main(String[] args) {
