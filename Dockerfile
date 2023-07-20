c79acc2b7be91b145480e288543dfb04590dbc95
choihana <chhana0703@gmail.com>
2023-07-20 15:49:31 +0900

    feat:docker 설정

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM openjdk:17
-ARG JAR_FILE=*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+LABEL maintainer="soolo"
+VOLUME /main-app
+ADD build/libs/SooloChat-0.0.1-SNAPSHOT.jar app.jar
+EXPOSE 5501
+ENTRYPOINT ["java","-Dspring.data.mongodb.uri=mongodb+srv://soolo:soolo@cluster0.nnm1vfo.mongodb.net/sooloChat?retryWrites=true&w=majority","-jar","app.jar"]
---
+++ src/main/java/com/soolo/soolochat/config/MongoDBConfig.java
@@ -0,0 +1,32 @@
+package com.soolo.soolochat.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoDBConfig {
+	// MongoDB 에 "_class" 들어가지 않게 설정
+	@Autowired
+	private MongoMappingContext mongoMappingContext;
+
+	@Bean
+	public MappingMongoConverter mappingMongoConverter(
+		MongoDatabaseFactory mongoDatabaseFactory,
+		MongoMappingContext mongoMappingContext
+	) {
+		DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDatabaseFactory);
+		MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+		converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+		return converter;
+	}
+}
